7dea11dde1ded7754bd796cbbf8743508ff6bd61
Arof2 <korbitorwart@gmail.com>
2023-01-22 18:21:06 +0100

    fix

--- PinguJumper/Assets/Scripts/Cave/doorP.cs
+++ PinguJumper/Assets/Scripts/Cave/doorP.cs
@@ -5,24 +5,24 @@ using UnityEngine;
 
 public class doorP : MonoBehaviour
 {
-    private MeshRenderer renderer;
+    private MeshRenderer rend;
     public float speed = 5;
     private Collider col;
     private void Start()
     {
-        renderer = GetComponent<MeshRenderer>();
+        rend = GetComponent<MeshRenderer>();
         col = GetComponent<Collider>();
         col.enabled = true;
     }
 
     public void melt()
     {
-        Material mat = renderer.material;
-        renderer.material.color = Color.Lerp(mat.color,
+        Material mat = rend.material;
+        rend.material.color = Color.Lerp(mat.color,
             new Color(mat.color.r, mat.color.g, mat.color.b, 0),
             Time.deltaTime * 5);
 
-        if (renderer.material.color.a < 5)
+        if (rend.material.color.a < 5)
         {
             gameObject.SetActive(false);
         }
